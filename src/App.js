490924c5817519461be0d1895c1b2cb7fa9c0f70
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2021-10-20 22:00:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignupForm from './components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      <br /><br />
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
